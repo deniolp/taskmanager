f290bf7c795ab1e838202a1a3b496d6aca438e1a
popov7 <deniolp@gmail.com>
2019-02-19 12:20:46 +0400

    Adding another one interpolation to getFilterElement function

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const getFilterElement = (name, amount, isChecked = false, isDisabled = false) =>
-  `<input type="radio" id="filter__${name.toLowerCase}"
-class="filter__input visually-hidden"
-name="filter"
-${isChecked ? ` checked` : ``}
-${isDisabled ? ` disabled` : ``}/>
-<label for="filter__${name.toLowerCase}" class="filter__label">
-${name.toUpperCase} <span class="filter__all-count">${amount}</span></label>
+const getFilterElement = (name, amount, isChecked = false, isDisabled = false) => `
+  <input type="radio" id="filter__${name.toLowerCase}"
+  class="filter__input visually-hidden"
+  name="filter"
+  ${isChecked ? ` checked` : ``}
+  ${isDisabled ? ` disabled` : ``}
+  />
+  <label for="filter__${name.toLowerCase}" class="filter__label">
+  ${name.toUpperCase} <span class="filter__${name.toLowerCase}-count">${amount}</span>
+  </label>
 `;
