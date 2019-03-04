79f6aea4f6cced9e9f6cf5fde455a3b69fe52939
popov7 <deniolp@gmail.com>
2019-03-04 23:22:23 +0400

    Creating getter template in class task

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,3 @@ export const getRandomNumber = (first = 0, second = 20) => {
   const max = Math.ceil(second);
   return Math.round(Math.random() * (max - min) + min);
 };
-
-export const isRepeated = (obj) => Object.values(obj).find((item) => item === true);
