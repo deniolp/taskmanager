f22ad79a5209ddf59c1da8dca60d8be22d26741d
popov7 <deniolp@gmail.com>
2019-03-05 16:22:35 +0400

    Correcting function of getting random number (utils.js)

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const getRandomNumber = (first = 0, second = 20) => {
   const min = Math.floor(first);
   const max = Math.ceil(second);
-  return Math.round(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
