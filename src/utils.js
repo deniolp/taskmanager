15ad03fef23eb0129e1d8b298489938c6082dc5c
popov7 <deniolp@gmail.com>
2019-03-22 17:03:24 +0300

    Deleting unnecessary modules

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export const getRandomNumber = (first = 0, second = 20) => {
-  const min = Math.floor(first);
-  const max = Math.ceil(second);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
